1557cb319497996b6d830824258a693b8b72c2a7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-04 12:49:10 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,9 +3,37 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
 
-$html = scraperwiki::scrape("http://google.com/);
-$dom = new simple_html_dom();
- $dom->load($html);
-echo $dom;
+$site = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
+//This is for Pagination 
+for($page = 0; $page < 1; $page++)
+    {
+        $FinalURL = $site . $page;
+        $html = file_get_html($FinalURL);
+    }
 
+if($html ==true)
+{
+//This is for Table Scrapping
+foreach($html->find("//*[@id='w0']/table/tbody/tr") as $element) 
+	{
+
+		if($element)
+	{
+
+	echo 	$num   = $element->find("td", 0);
+	echo	$courtname  = $element->find("td", 1);
+	echo	$caseno  = $element->find("td", 2);
+	echo	$status  = $element->find("td", 3);
+	$href = $element->find(".//td/button", 0);
+
+	if(is_object($href))
+	{
+		 $urlbutton = $href->value;
+		 echo $urlbutton;
+	}
+	}
+	}}
+	else{
+		$html = file_get_html($FinalURL);
+	}
 ?>
