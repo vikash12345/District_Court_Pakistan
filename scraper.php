8383664af43cb3a1e31e818c39ad4c4f4defa7fe
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-04 12:41:38 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,28 +7,5 @@ $html = scraperwiki::scrape("http://google.com/);
 $dom = new simple_html_dom();
  $dom->load($url);
 echo $dom;
-/*
-for ($innerloop = 1; $innerloop <2; $innerloop++) 
-			{
-				$NewLink	=	$url  . $innerloop;
-				$html 		= file_get_html($NewLink);
-}
 
-foreach($html->find("//*[@id='w0']/table/tbody/tr") as $element) 
-			{
-
-				if($element)
-				{
-					echo 	$num   = $element->find("td", 0);
-
-					
-					
-					
-					
-					
-					
-					
-				}
-}
-*/
 ?>
