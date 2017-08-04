fed4532c1cfcedf2a39d1c16ff289b48edb6f97d
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-04 12:31:24 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,21 +9,13 @@ for ($innerloop = 1; $innerloop <2; $innerloop++)
 				$html 		= file_get_html($NewLink);
 }
 
-foreach($html->find("/html/body/div/table/tbody/tr[1]/td/form/table/tbody/tr[7]/td/table/tbody") as $element)
+foreach($html->find("//*[@id='w0']/table/tbody/tr") as $element) 
 			{
 
 				if($element)
 				{
-					echo $reg_no 		= $element->find("tr/td[2]/font" ,1);
-					echo $reg_date 		= $element->find("tr/td[2]/font" ,2);
-					echo $maker 		= $element->find("tr/td[2]/font" ,4);
-					echo $model 		= $element->find("tr/td[2]/font" ,6);
-					echo $chassis_no	= $element->find("tr/td[2]/font" ,8);
-					echo $engine_no 	= $element->find("tr/td[2]/font" ,10);
-					echo $owner 		= $element->find("tr/td[2]/font" ,12);
-					echo $sw 			= $element->find("tr/td[2]/font" ,14);
-					echo $type 			= $element->find("tr/td[2]/font" ,16); 
-					echo $NewLink;
+					echo 	$num   = $element->find("td", 0);
+
 					
 					
 					
