f6fbee54f7003750c1a92853ee76fd07722b581c
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-04 12:27:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,56 +1,13 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-
-
-
-//
-// // Read in a page
-
-
-require 'scraperwiki/simple_html_dom.php';
+<?php
 require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 
+$url = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
+for ($innerloop = 100; $innerloop <9999; $innerloop++) 
+			{
+				$NewLink	=	$url  . $innerloop;
+				$html 		= file_get_html($NewLink);
+}
 
-
-$site = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
-//This is for Pagination 
-for($page = 1; $page < 3; $page++)
-    {
-        $FinalURL = $site . $page;
-        $abc = file_get_html($FinalURL);
-    }
-
-
-$html = scraperwiki::scrape($abc);
-//
-// // Find something on the page using css selectors
-$dom = new simple_html_dom();
-$dom->load($html);
-
- echo  $html
-     
-     ?>
+echo $html;
+?>
