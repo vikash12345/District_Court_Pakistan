fccb7936da2aaa5de704a798e89ef64660a337a8
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-04 12:28:02 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
 $url = 'http://202.61.43.40:8080/index.php?r=site%2Fsearchbyvalue&page=';
-for ($innerloop = 100; $innerloop <9999; $innerloop++) 
+for ($innerloop = 1; $innerloop <2; $innerloop++) 
 			{
 				$NewLink	=	$url  . $innerloop;
 				$html 		= file_get_html($NewLink);
